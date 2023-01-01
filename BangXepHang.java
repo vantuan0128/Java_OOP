e8b70e85a269772d7408d0e9a03c19585f42a15b
Tuan Nguyen <121681379+vantuan0128@users.noreply.github.com>
2023-01-01 16:50:58 +0700

    Add files via upload

--- BangXepHang.java
+++ BangXepHang.java
@@ -1,4 +1,4 @@
-package CodePTIT;
+package OOP_2023;
 
 import java.util.*;
 
@@ -6,37 +6,37 @@ public class BangXepHang {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
         int n = Integer.parseInt(sc.nextLine());
-        List <SinhVien4> list = new ArrayList<>();
+        List <SinhVien7> list = new ArrayList<>();
         for (int i = 0; i < n; ++i){
             String name = sc.nextLine();
             String[] s = sc.nextLine().split(" ");
             int correct = Integer.parseInt(s[0]);
             int submit = Integer.parseInt(s[1]);
-            SinhVien4 x = new SinhVien4(name, correct, submit);
+            SinhVien7 x = new SinhVien7(name, correct, submit);
             list.add(x);
         }
-        Collections.sort(list, new Comparator<SinhVien4>() {
+        Collections.sort(list, new Comparator<SinhVien7>() {
             @Override
-            public int compare(SinhVien4 o1, SinhVien4 o2) {
+            public int compare(SinhVien7 o1, SinhVien7 o2) {
                 return o1.getName().compareTo(o2.getName());
             }
         });
-        Collections.sort(list, new Comparator<SinhVien4>() {
+        Collections.sort(list, new Comparator<SinhVien7>() {
             @Override
-            public int compare(SinhVien4 o1, SinhVien4 o2) {
+            public int compare(SinhVien7 o1, SinhVien7 o2) {
                 if (o1.getCorrect() != o2.getCorrect()) return o2.getCorrect() - o1.getCorrect();
                 return o1.getSubmit() - o2.getSubmit();
             }
         });
-        for (SinhVien4 x : list) System.out.println(x);
+        for (SinhVien7 x : list) System.out.println(x);
     }
 }
 
-class SinhVien4{
+class SinhVien7{
     private String name;
     private int correct, submit;
 
-    public SinhVien4(String name, int correct, int submit) {
+    public SinhVien7(String name, int correct, int submit) {
         this.name = name;
         this.correct = correct;
         this.submit = submit;
